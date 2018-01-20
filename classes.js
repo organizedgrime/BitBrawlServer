cd2f7cdb3a3b2252b352ebf438d78d0a94b22d30
nico gonzalez <nicolasnicogonzalez@gmail.com>
2018-01-20 10:40:53 -0500

    generating test decks

--- classes.js
+++ classes.js
@@ -7,7 +7,9 @@ module.exports = {
 			this.fortitude = fortitude;
 			this.rarity = rarity;
 
-			this.toString = function() { return this.name + ":" + this.type + ":" + this.attack + ":" + this.fortitude + ":" + this.rarity};
+			this.toString = function() { 
+				return this.name + ":" + this.type + ":" + this.attack + ":" + this.fortitude + ":" + this.rarity + "\n";
+			};
 		}
 	},
 
@@ -16,12 +18,27 @@ module.exports = {
 			this.stash = cards;
 			this.hand = null;
 			this.play = null;
+
+			this.toString = function() {
+				var str = "";
+				for(var i = 0; i < stash.length; i++) {
+					str += stash[i].toString();
+				}
+				return str;
+			};
 		}
 	},
 
 	board: class {
 		constructor(decks) {
 			this.decks = decks;
+
+			this.toString = function() {
+				var str = "";
+				str += "DECK 1\n" + decks.deck1.toString() + "\n";
+				str += "DECK 2\n" + decks.deck2.toString() + "\n";
+				return str;
+			}
 		}
 	},
 
--- main.js
+++ main.js
@@ -5,6 +5,29 @@ var app = express();
 var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
 
+// set up mock deck
+var p1cards = [], p2cards = [];
+for(var i = 0; i < 10; i++) {
+    var card = new classes.card(
+        Math.random().toString(36).substring(7), 
+        classes.type[Math.round(Math.random() * 3)], 
+        Math.round(Math.random() * 100), Math.round(Math.random() * 100), 
+        classes.rarity[Math.round(Math.random() * 5)]
+    );
+    p1cards.push(card);
+
+    var card2 = new classes.card(
+        Math.random().toString(36).substring(7), 
+        classes.type[Math.round(Math.random() * 3)], 
+        Math.round(Math.random() * 100), Math.round(Math.random() * 100), 
+        classes.rarity[Math.round(Math.random() * 5)]
+    );
+    p2cards.push(card2);
+}
+
+var board = new classes.board({deck1: p1cards, deck2: p2cards});
+console.log(board.toString());
+
 // set stuff up
 app.use(express.static(__dirname + '/node_modules'));  
 app.get('/', function(req, res,next) {
